c755c4a26b8c24cbee1686cdd7234cc6d46e5a1d
Jan Randa <jnranda@gmail.com>
2025-07-23 02:23:23 +0200

    BF: beam reset doesnt trigger tx

--- src/server/router/SocketsGroup.js
+++ src/server/router/SocketsGroup.js
@@ -130,7 +130,7 @@ export class SocketsGroup {
         const txStatuses = ["init", "ready", "expired"];
         vault.on(async ({status, data}, groupId, sourceSocket)=>{
             if (!txStatuses.includes(status)) { return; }
-            if (!_p.byId(groupId)?.size) { return; }
+            if (!_p.byId.get(groupId)?.size) { return; }
             if (status !== "ready" && vault.hasRemote) { return vault.get(groupId, socket); }
             if (!sourceSocket) { return this.tx(channel, groupId, data); }
             else { return this.txBroad(channel, data, sourceSocket); }
